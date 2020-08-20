71a6c9e84c1a92370c2ddb33944195676194c796
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-08-20 14:53:21 +0300

    work count on heroku

--- avto/storage.py
+++ avto/storage.py
@@ -5,17 +5,20 @@ class Counter:
     key = 'Count_key'
 
     def __init__(self):
+        print('init counter')
         try:
+            print('try get count')
             self.key = MyCount.objects.get(stolb='count')
         except:
+            print('exept, create count')
             self.key = MyCount.objects.create(stolb = 'count', m_coun = 0)
 
     def inc(self):
-
+        print('inc')
         self.key.m_coun = self.key.m_coun + 1
         self.key.save()
 
         return self.key.m_coun
 
-
+print('make counter')
 counter = Counter()
--- avto/views.py
+++ avto/views.py
@@ -12,9 +12,11 @@ class Start(TemplateView):
     template_name = 'avto/index.html'
 
     def get_context_data(self, **kwargs):
+        print('start index')
         data = super().get_context_data(**kwargs)
 
         data['counter'] = counter.inc()
+        print(data)
         return data
 
 
