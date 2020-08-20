91a49957ededffde7197ba293fbbee6fe76cdfce
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-08-20 14:21:26 +0300

    work count on heroku

--- avto/urls.py
+++ avto/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('', views.Start.as_view(), name='index'),
+    path('/', views.Start.as_view(), name='index'),
     path('add', views.Add_avto.as_view(), name='add'),
     path('view', views.AvtoView.as_view(), name='view'),
 
