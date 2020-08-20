c35d7a6e88151ffbe97fe116ce251c2f8e9cf10d
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-08-20 16:50:10 +0300

    work count on heroku test

--- heroku/settings.py
+++ heroku/settings.py
@@ -116,18 +116,14 @@ if CLEARDB_DATABASE_URL:
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 4,
+        }
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+
 ]
 
 
