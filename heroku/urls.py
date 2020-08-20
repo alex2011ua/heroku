9a5e2907370816972a9ef807c70f8b4f5873b59f
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-08-20 16:29:25 +0300

    work count on heroku test

--- heroku/urls.py
+++ heroku/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     path('', include('avto.urls')),
     path('avto/', include('avto.urls')),
