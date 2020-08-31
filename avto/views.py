04185d6a8424c28e5df7719c24240a247974d59b
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-08-31 10:44:40 +0300

    add import-export

--- avto/views.py
+++ avto/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from avto.models import Avto
 from avto.get_add_avto import add_avto, get_avto
-
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.views import View
 from .counts import my_count
+
 
 class AvtoDellConfirm(LoginRequiredMixin, View):
     @staticmethod
@@ -18,8 +17,6 @@ class AvtoDellConfirm(LoginRequiredMixin, View):
         }
         context.update(count)
         avto.delete()
-
-
         return render(request, "avto/form_dell_confirm.html", context)
 
 
@@ -29,7 +26,6 @@ class AvtoDell(LoginRequiredMixin, View):
         print(nomber)
         print(request.user.username)
         avto = Avto.objects.get(nomer_avto = nomber)
-
         context = {
             'nomer': nomber,
             'discr': avto.discript_avto
@@ -48,8 +44,6 @@ class AddAvto(LoginRequiredMixin, View):
         nomer_avto = request.POST.get("nomer_avto").strip()
         discript_avto = request.POST.get("discript_avto")
         avtos = Avto.objects.filter(nomer_avto = nomer_avto)
-
-
         if len(avtos) == 0:
             Avto.objects.create(nomer_avto=nomer_avto,
                                 discript_avto=discript_avto,
@@ -61,7 +55,6 @@ class AddAvto(LoginRequiredMixin, View):
 class AvtoView(LoginRequiredMixin, View):
     @staticmethod
     def get(request):
-
         context = my_count()
         return render(request, "avto/form_search.html", context)
 
@@ -70,11 +63,9 @@ class AvtoView(LoginRequiredMixin, View):
         context = my_count()
         nomer_avto = request.POST.get("search").strip()
         avtos = Avto.objects.filter(nomer_avto__icontains = nomer_avto)
-
         context.update({
             'zapros': nomer_avto,
             'avtos': avtos,
-
             "count_search": len(avtos),
         })
 
