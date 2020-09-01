441f8ba5689a4fd7f0822f77d26d7a46e03913fd
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-09-01 16:14:01 +0300

    add export logs

--- index/views.py
+++ index/views.py
@@ -1,6 +1,10 @@
 from .storage import counter
 from django.views.generic import TemplateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from index.logg import functionss
+from index.models import MyLogg
+import os
 
 
 class Start(TemplateView):
@@ -12,3 +16,29 @@ class Start(TemplateView):
         data['counter'] = counter()
         return data
 
+class Export(LoginRequiredMixin, TemplateView):
+    @staticmethod
+    @functionss
+    def get(request):
+        file_name = 'temp_log.xml'
+        log = MyLogg.objects.all()[0:100]
+        with open(file_name, 'w', encoding = 'utf8') as file:
+            for logginn in log:
+                data = str(logginn.date_time)
+                text = logginn.text
+                context = logginn.context or ''
+                user = str(logginn.user) or ''
+
+                file.write(data[0:19] + ";" + text + ";" + context + ";" + user + "\n")
+
+        fp = open(file_name, "rb")
+        response = HttpResponse(fp.read())
+        fp.close()
+
+        file_type = 'application/xml'
+
+        response['Content-Type'] = file_type
+        response['Content-Length'] = str(os.stat(file_name).st_size)
+        response['Content-Disposition'] = "attachment; filename=report.xml"
+
+        return response
