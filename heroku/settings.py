5587c3133eda18e5ded8e004faebe918104a1a27
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-09-18 18:01:35 +0300

    add requests

--- heroku/settings.py
+++ heroku/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'n8ga2_ghg!d9wpil^&tl69vo6t%6_ufst0lg-8#fx%xa*s*sxp')
-
+APPID_Weather = os.getenv('OpenWeatherMap', 'd16e0dacb5474e43829b385c7102e12d')
 
 CLEARDB_DATABASE_URL = os.getenv("CLEARDB_DATABASE_URL", False)
 # SECURITY WARNING: don't run with debug turned on in production!
