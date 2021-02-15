204fdfa1c9f8a644bd495ad1aeb73b3316efa191
Aleksey Kovalenko <alex2011ua@gmail.com>
2021-02-15 10:13:15 +0200

    restart

--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-
     path('', views.Start.as_view(), name='index'),
     path('index/export/', views.Export.as_view(), name='export'),
-
-
-
 ]
